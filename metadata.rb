b2077f81d71a5b3d93903e5504c30ade391c7b60
Or Cohen <orc@fewbytes.com>
2013-01-02 22:52:41 +0200

    Updated version to 0.1.1

--- metadata.rb
+++ metadata.rb
@@ -1,9 +1,9 @@
 maintainer       "Fewbytes"
 maintainer_email "chef@fewbytes.com"
 license          "All rights reserved"
-description      "Installs/Configures check_mk"
+description      "Installs/Configures Check_MK"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.0"
+version          "0.1.1"
 
 depends "apache2"
 depends "cluster_service_discovery"
