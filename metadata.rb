2d4afd258d6bc7bec899893d1a6c4234c9e64393
Or Cohen <orc@fewbytes.com>
2013-04-08 15:38:17 +0300

    Bumped patch number

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "chef@fewbytes.com"
 license          "All rights reserved"
 description      "Installs/Configures Check_MK"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.4.0"
+version          "0.4.1"
 
 depends "apache2"
 depends "cluster_service_discovery"
