0d0cb92669d81a5cbf26d95254e6ab1326a55149
Or Cohen <orc@fewbytes.com>
2013-04-17 00:33:08 +0300

    Fixed foodcritic FC003 (server-specific features)

--- recipes/multisite.rb
+++ recipes/multisite.rb
@@ -1,12 +1,18 @@
 include_recipe "xinetd"
 
+check_mk_servers = if Chef::Config[:solo]
+                    Chef::Log.warn("This recipe uses search. Chef Solo does not support search.")
+                  else
+                    search(:node, 'cluster_services:check-mk-server').map { |n| n.ip_for_node(node) }
+                  end
+
 template "/etc/xinetd.d/livestatus" do
   source "livestatus.xinetd.erb"
   owner "root"
   group "root"
   mode "0644"
   variables(
-    :only_from => search(:node, 'cluster_services:check-mk-server').map { |n| n.ip_for_node(node) },
+    :only_from => check_mk_servers,
     :nagios_user => node['check_mk']['server']['user'],
     :unix_socket => node['check_mk']['server']['paths']['livestatus_unix_socket']
   )
