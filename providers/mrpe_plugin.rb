d61097d84f5079af479db8121dc98eebdd7a68fd
Or Cohen <orc@fewbytes.com>
2013-04-28 17:22:47 +0300

    Changed the way updated_by_last_action is handled (coupled with template)

--- providers/mrpe_plugin.rb
+++ providers/mrpe_plugin.rb
@@ -2,14 +2,12 @@ action :create do
   # Template file
   template_file = node['check_mk']['agent']['mrpe']
 
-  # Flag 
-
   # The magic!
   begin
     t = resources(:template => template_file)
 
     # Warn if we are about to override a previously configured MRPE plugin
-    log "Check_MK MRPE plugin was overridden #{@}" do
+    log "Check_MK MRPE plugin '#{new_resource.plugin} was overridden" do
       level :warn
       only_if { t.variables[:plugins][new_resource.plugin] }
     end
@@ -29,5 +27,19 @@ action :create do
     :arguments => params[new_resource.arguments]
   }
 
-  new_resource.updated_by_last_action(true)
+  new_resource.updated_by_last_action(@current_mrpe_cfg.find {|s| s == new_resource_expected_line})
+end
+
+def load_current_resource
+  @current_mrpe_cfg = read_mrpe_cfg
+end
+
+def new_resource_expected_line
+  "#{new_resource.plugin} #{new_resource.path} #{new_resource.arguments}\n"
+end
+
+def read_mrpe_cfg
+  ::File.open node['check_mk']['agent']['mrpe'] do |file|
+    file.readlines
+  end
 end
