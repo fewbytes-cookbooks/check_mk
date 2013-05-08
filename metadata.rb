9b167e77f8e5d18b46a89f69464d7127fc02398b
Or Cohen <orc@fewbytes.com>
2013-05-08 19:06:56 +0300

    Bumped version for development

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "chef@fewbytes.com"
 license          "All rights reserved"
 description      "Installs/Configures Check_MK"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.4.2"
+version          "1.0.0"
 
 depends "apache2"
 depends "xinetd"
