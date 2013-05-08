31124a56570c53188c237d6b214254ddee64079e
Or Cohen <orc@fewbytes.com>
2013-05-08 19:02:57 +0300

    Agent now downloads a package defined in attributes

--- attributes/agent.rb
+++ attributes/agent.rb
@@ -2,3 +2,9 @@ default['check_mk']['agent']['plugins'] = '/usr/lib/check_mk_agent/plugins'
 default['check_mk']['agent']['local'] = '/usr/lib/check_mk_agent/local'
 default['check_mk']['agent']['conf_dir'] = '/etc/check_mk'
 default['check_mk']['agent']['mrpe'] = '/etc/check_mk/mrpe.cfg'
+
+case platform
+when "debian", "ubuntu"
+  default["check_mk"]["agent"]["package"]["url"] = "http://mathias-kettner.de/download/check-mk-agent_1.2.2p1-2_all.deb"
+  default["check_mk"]["agent"]["package"]["checksum"] = "91f839624e6e75154655c8276e04cc9521256f43e0f2ac8528f588ddddbe9899"
+end
--- recipes/agent.rb
+++ recipes/agent.rb
@@ -1,7 +1,31 @@
+require 'uri'
+
 include_recipe "xinetd"
 
+cmk_package_uri = URI.parse(node['check_mk']['agent']['package']['url'])
+cmk_package = File.join("/tmp", File.basename(cmk_package_uri.path))
+
+remote_file cmk_package do
+  action :create
+  backup 5
+  owner "root"
+  group "root"
+  mode "0644"
+  source cmk_package_uri.to_s
+  checksum node['check_mk']['agent']['package']['checksum']
+end
+
 package "check-mk-agent" do
-  action :install
+  source cmk_package
+  provider case File.extname(cmk_package)
+            when ".deb"
+              Chef::Provider::Package::Dpkg
+            when ".rpm"
+              Chef::Provider::Package::Rpm
+            else
+              Chef::Provider::Package
+            end
+    
 end
 
 check_mk_servers = if Chef::Config[:solo]
