955311e1564ddd1aaf780bc9ba9739830ac39191
Or Cohen <orc@fewbytes.com>
2013-06-22 23:57:13 +0300

    Added download attributes for RPM based linux

--- attributes/agent.rb
+++ attributes/agent.rb
@@ -4,7 +4,10 @@ default['check_mk']['agent']['conf_dir'] = '/etc/check_mk'
 default['check_mk']['agent']['mrpe'] = '/etc/check_mk/mrpe.cfg'
 
 case platform
-when "debian", "ubuntu"
-  default["check_mk"]["agent"]["package"]["url"] = "http://mathias-kettner.de/download/check-mk-agent_1.2.2p1-2_all.deb"
-  default["check_mk"]["agent"]["package"]["checksum"] = "91f839624e6e75154655c8276e04cc9521256f43e0f2ac8528f588ddddbe9899"
+  when "debian", "ubuntu"
+    default["check_mk"]["agent"]["package"]["url"] = "http://mathias-kettner.de/download/check-mk-agent_1.2.2p1-2_all.deb"
+    default["check_mk"]["agent"]["package"]["checksum"] = "91f839624e6e75154655c8276e04cc9521256f43e0f2ac8528f588ddddbe9899"
+  when "centos", "redhat", "amazon", "scientific"
+    default["check_mk"]["agent"]["package"]["url"] = "http://mathias-kettner.de/download/check_mk-agent-1.2.2p1-1.noarch.rpm"
+    default["check_mk"]["agent"]["package"]["checksum"] = "ee257b31841c1f33a5d14f2eb01128a6b1275c880b5f333e0e0130717353f7a6"
 end
