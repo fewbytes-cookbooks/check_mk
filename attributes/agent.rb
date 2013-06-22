4f7b68298650352be90766ff0be4ec9672cfe084
Or Cohen <orc@fewbytes.com>
2013-06-22 23:58:09 +0300

    Added agent port as attribute and in template

--- attributes/agent.rb
+++ attributes/agent.rb
@@ -2,6 +2,7 @@ default['check_mk']['agent']['plugins'] = '/usr/lib/check_mk_agent/plugins'
 default['check_mk']['agent']['local'] = '/usr/lib/check_mk_agent/local'
 default['check_mk']['agent']['conf_dir'] = '/etc/check_mk'
 default['check_mk']['agent']['mrpe'] = '/etc/check_mk/mrpe.cfg'
+default['check_mk']['agent']['port'] = 6556
 
 case platform
   when "debian", "ubuntu"
