15c0c605bff2348dddc4e007b12254e8f535fd9a
Or Cohen <orc@fewbytes.com>
2013-06-23 18:02:43 +0300

    Added test for agent port

--- files/default/tests/minitest/agent_test.rb
+++ files/default/tests/minitest/agent_test.rb
@@ -1,5 +1,18 @@
+require 'socket'
+
 class TestAgent < MiniTest::Chef::TestCase
   def test_agent_listen
-    assert TCPSocket.new('localhost', node['check_mk']['agent']['port'])
+
+    count = 0
+    begin
+      TCPSocket.new('localhost', node['check_mk']['agent']['port'])
+    rescue Errno::ECONNREFUSED => e
+      if count < 3
+        count += 1; sleep 1; retry
+      else
+        raise e
+      end
+    end
+
   end
 end
