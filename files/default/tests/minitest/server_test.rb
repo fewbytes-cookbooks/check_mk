97fe9844cba1888073bfe1e3485d426bd97b7024
Or Cohen <orc@fewbytes.com>
2013-06-25 12:34:48 +0300

    Added test for check_mk server install

---
+++ files/default/tests/minitest/server_test.rb
@@ -0,0 +1,14 @@
+require 'open3'
+
+class TestAgent < MiniTest::Chef::TestCase
+  def test_livestatus_version
+    version = node["check_mk"]["server"]["package"]["version"]
+    socket = node["check_mk"]["server"]["paths"]["livestatus_unix_socket"]
+
+    stdin, stdout, stderr = Open3.popen3("unixcat #{socket}")
+    stdin.puts "GET status\nColumns: livestatus_version"
+    stdin.close
+
+    assert version == stdout.read.chop
+  end
+end
