fce0057ccc32b796108f4d21297b16d5a140c98f
Or Cohen <or.cohen007@gmail.com>
2013-08-03 23:48:42 +0300

    Created a special recipe to install nagios from source

---
+++ recipes/nagios.rb
@@ -0,0 +1,123 @@
+include_recipe 'ark'
+include_recipe 'check_mk::apache2'
+
+include_recipe 'php'
+include_recipe 'php::module_gd'
+
+# Create the nagios group
+group node['check_mk']['nagios']['group'] do
+  system true
+end
+
+# Create the nagios user
+user node['check_mk']['nagios']['user'] do
+  gid node['check_mk']['nagios']['group']
+  system true
+  shell '/bin/false'
+end
+
+# Create the configuration directory
+dir_etc = node['check_mk']['nagios']['dir']['sysconf']
+directory dir_etc do
+  owner node['check_mk']['nagios']['user']
+  group node['check_mk']['nagios']['group']
+  mode "0755"
+  action :create
+  recursive true
+end
+
+# Download and unpack
+ark 'nagios' do
+  url node['check_mk']['nagios']['package']['url']
+  version node['check_mk']['nagios']['package']['version']
+  checksum node['check_mk']['nagios']['package']['checksum']
+end
+
+# Compile and install
+execute 'nagios configure' do
+  command "./configure --prefix=#{node['check_mk']['nagios']['dir']['prefix']} \
+    --mandir=#{node['check_mk']['nagios']['dir']['man']} \
+    --bindir=#{node['check_mk']['nagios']['dir']['bin']} \
+    --sbindir=#{node['check_mk']['nagios']['dir']['sbin']} \
+    --datadir=#{node['check_mk']['nagios']['dir']['data']} \
+    --sysconfdir=#{node['check_mk']['nagios']['dir']['sysconf']} \
+    --infodir=#{node['check_mk']['nagios']['dir']['info']} \
+    --libexecdir=#{node['check_mk']['nagios']['dir']['libexec']} \
+    --localstatedir=#{node['check_mk']['nagios']['dir']['localstate']} \
+    --enable-event-broker \
+    --with-nagios-user=#{node['check_mk']['nagios']['user']} \
+    --with-nagios-group=#{node['check_mk']['nagios']['group']} \
+    --with-command-user=#{node['check_mk']['nagios']['user']} \
+    --with-command-group=#{node['check_mk']['nagios']['group']} \
+    --with-init-dir=#{node['check_mk']['nagios']['dir']['init.d']} \
+    --with-lockfile=#{node['check_mk']['nagios']['path']['lockfile']} \
+    --with-mail=/usr/bin/mail \
+    --with-perlcache \
+    --with-htmurl=#{node['check_mk']['nagios']['www']['html']} \
+    --with-cgiurl=#{node['check_mk']['nagios']['www']['cgi']}"
+  cwd "#{node['ark']['prefix_root']}/nagios"
+  creates 'Makefile'
+end
+
+execute 'nagios make all' do
+  command 'make all'
+  cwd "#{node['ark']['prefix_root']}/nagios"
+  creates 'base/nagios'
+end
+
+execute 'nagios make install' do
+  command 'make install'
+  cwd "#{node['ark']['prefix_root']}/nagios"
+  not_if "#{node['check_mk']['nagios']['path']['nagios']} |
+grep 'Nagios Core #{node['check_mk']['nagios']['package']['version']}'"
+end
+
+execute 'nagios make install-init' do
+  command 'make install-init'
+  cwd "#{node['ark']['prefix_root']}/nagios"
+  not_if { ::File.exist?("/etc/init.d/nagios") }
+end
+
+directory dir_etc do
+  owner node['check_mk']['nagios']['user']
+  group node['check_mk']['nagios']['group']
+  mode '0755'
+end
+
+template node['check_mk']['nagios']['path']['cgi.cfg'] do
+  owner node['check_mk']['nagios']['user']
+  group node['check_mk']['nagios']['group']
+  mode '0644'
+  notifies :reload, 'service[nagios]'
+end
+
+template node['check_mk']['nagios']['path']['nagios.cfg'] do
+  owner node['check_mk']['nagios']['user']
+  group node['check_mk']['nagios']['group']
+  mode '0644'
+  notifies :reload, 'service[nagios]'
+end
+
+
+# Nagios plugins
+
+# Download and unpack
+ark 'nagios-plugins' do
+  url node['check_mk']['nagios']['plugins']['url']
+  version node['check_mk']['nagios']['plugins']['version']
+  checksum node['check_mk']['nagios']['plugins']['checksum']
+end
+
+# Compile and install
+execute 'nagios-plugins configure' do
+  command "./configure --with-nagios-user=#{node['check_mk']['nagios']['user']} \
+    --with-nagios-group=#{node['check_mk']['nagios']['group']}"
+  cwd "#{node['ark']['prefix_root']}/nagios-plugins"
+  creates 'Makefile'
+end
+
+execute 'nagios-plugins make and install' do
+  command 'make && make install'
+  cwd "#{node['ark']['prefix_root']}/nagios-plugins"
+  creates "#{node['check_mk']['nagios']['plugins']['dir']['libexec']}/check_icmp"
+end
