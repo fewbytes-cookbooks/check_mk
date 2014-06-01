650e60859ce08c9c6484a54c2edd0cf2a213cc3c
Pavel Suchman <pavel.suchman@gmail.com>
2014-06-01 15:16:23 +0300

    Updated agent URL to use the files from http://mathias-kettner.de/download/

--- attributes/agent.rb
+++ attributes/agent.rb
@@ -9,9 +9,9 @@ default['check_mk']['agent']['dir']['conf'] = '/etc/check_mk'
 
 case platform
   when "debian", "ubuntu"
-    default["check_mk"]["agent"]["package"]["url"] = "http://inneractive-deploy.s3.amazonaws.com/check_mk/check-mk-agent_1.2.4p3-2_all.deb"
+    default["check_mk"]["agent"]["package"]["url"] = "http://mathias-kettner.de/download/check-mk-agent_1.2.4p3-2_all.deb"
     default["check_mk"]["agent"]["package"]["checksum"] = "127fae672cd6c0ceb61a994460216e8f5e6e30d05e293319ca4f45ebd291219b"
   when "centos", "redhat", "amazon", "scientific"
-    default["check_mk"]["agent"]["package"]["url"] = "http://inneractive-deploy.s3.amazonaws.com/check_mk/check_mk-agent-logwatch-1.2.4p3-1.noarch.rpm"
-    default["check_mk"]["agent"]["package"]["checksum"] = "9c08666228ec84b3459af4edbe63620052ec64cddeeb407c25a0f76d6509aff6"
+    default["check_mk"]["agent"]["package"]["url"] = "http://mathias-kettner.de/download/check_mk-agent-1.2.4p3-1.noarch.rpm"
+    default["check_mk"]["agent"]["package"]["checksum"] = "8d9d591e03c8bcce63564ee59fb2317977a473a589e61f803d6b58c4d067cea9"
 end
