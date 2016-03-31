0b0f5520085c5984732496d07b6b3c730d888d8c
Or Cohen <orc@fewbytes.com>
2016-03-31 09:56:41 +0300

    Added Aapche 2.0 license

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name             "check_mk"
 maintainer       "Fewbytes"
 maintainer_email "chef@fewbytes.com"
-license          "All rights reserved"
+license          "Apache 2.0"
 description      "Installs/Configures Check_MK"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "1.0.3"
